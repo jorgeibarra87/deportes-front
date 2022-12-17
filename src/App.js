36a19043204357692f7038df11ec3282fc0fd664
jorgeibarra87 <jorgeibarra87@gmail.com>
2022-12-16 23:20:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Saludar() {
+  return <div>
+    <h1>Hola mundo</h1>
+    <h2>Mi primer react</h2>
+    <table border={1}>
+      <tr>
+        <td>
+          inicio
+        </td>
+        <td>
+          Nombre
+        </td>
+      </tr>
+    </table>
+
+  </div>
+}
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        {Saludar()}
+      </>
+    );
+  }
 }
 
 export default App;
